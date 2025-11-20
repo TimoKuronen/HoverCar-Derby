7a05fce656fd2fe8d88619a00359a1487303ffa3
TimoKuronen <timojkuronen@gmail.com>
2025-11-20 12:43:05 +0200

    Update visuals properly for all players

--- Assets/Limekicker/Scripts/CarColorPainter.cs
+++ Assets/Limekicker/Scripts/CarColorPainter.cs
@@ -1,61 +1,98 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class CarColorPainter : MonoBehaviour
+public class CarColorPainter : NetworkBehaviour
 {
+    [Header("Visual Variants")]
     [SerializeField] private GameObject[] carBody;
+
+    [Header("Palettes")]
     [SerializeField] private CarColorPalette carColorPalette;
+
+    [Header("Parts")]
     [SerializeField] private List<MeshRenderer> primaryPartRenderers = new List<MeshRenderer>();
     [SerializeField] private List<MeshRenderer> secondaryPartRenderers = new List<MeshRenderer>();
     [SerializeField] private List<MeshRenderer> decorationPartRenderers = new List<MeshRenderer>();
     [SerializeField] private List<MeshRenderer> bumperPartRenderers = new List<MeshRenderer>();
 
-    public void AddCarPart(ColorType colorType, MeshRenderer partType)
+    private NetworkVariable<int> colorIndex = new NetworkVariable<int>(
+        0,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server
+    );
+
+    public override void OnNetworkSpawn()
     {
-        switch (colorType)
-        {
-            case ColorType.Primary:
-                primaryPartRenderers.Add(partType);
-                break;
-            case ColorType.Secondary:
-                secondaryPartRenderers.Add(partType);
-                break;
-            case ColorType.Decoration:
-                decorationPartRenderers.Add(partType);
-                break;
-            case ColorType.Bumper:
-                bumperPartRenderers.Add(partType);
-                break;
-        }
+        colorIndex.OnValueChanged += OnColorChanged;
+
+        // Apply immediately also for late joiners
+        OnColorChanged(0, colorIndex.Value);
     }
 
     public void AssignColor(int index)
     {
-        Debug.Log("assigning color " + index);
-        for (int i = 0; i < carBody.Length; i++)
+        if (IsServer)
         {
-            carBody[i].SetActive(i == index);
+            colorIndex.Value = index;
         }
+        else
+        {
+            SetColorServerRpc(index);
+        }
+    }
 
-        carColorPalette = carColorPalette == null ? Resources.Load<CarColorPalette>("CarColorPalette") : carColorPalette;
+    [ServerRpc]
+    private void SetColorServerRpc(int index)
+    {
+        colorIndex.Value = index;
+    }
+
+    private void OnColorChanged(int oldIndex, int newIndex)
+    {
+        ApplyColor(newIndex);
+    }
+
+    private void ApplyColor(int index)
+    {
+        for (int i = 0; i < carBody.Length; i++)
+            carBody[i].SetActive(i == index);
+
+        if (carColorPalette == null)
+            carColorPalette = Resources.Load<CarColorPalette>("CarColorPalette");
+
         CarColors selectedColors = carColorPalette.GetCarColors(index);
 
-        foreach (var renderer in primaryPartRenderers)
+        // Apply all material colors
+        foreach (var r in primaryPartRenderers)
+            r.material.color = selectedColors.primaryColor;
+        foreach (var r in secondaryPartRenderers)
+            r.material.color = selectedColors.secondaryColor;
+        foreach (var r in decorationPartRenderers)
+            r.material.color = selectedColors.decorationColor;
+        foreach (var r in bumperPartRenderers)
+            r.material.color = selectedColors.bumperColor;
+    }
+
+    public void AddCarPart(ColorType colorType, MeshRenderer renderer)
+    {
+        switch (colorType)
         {
-            renderer.material.color = selectedColors.primaryColor;
-        }
-        foreach (var renderer in secondaryPartRenderers)
-        {
-            renderer.material.color = selectedColors.secondaryColor;
-        }
-        foreach (var renderer in decorationPartRenderers)
-        {
-            renderer.material.color = selectedColors.decorationColor;
-        }
-        foreach (var renderer in bumperPartRenderers)
-        {
-            renderer.material.color = selectedColors.bumperColor;
+            case ColorType.Primary:
+                primaryPartRenderers.Add(renderer);
+                break;
+
+            case ColorType.Secondary:
+                secondaryPartRenderers.Add(renderer);
+                break;
+
+            case ColorType.Decoration:
+                decorationPartRenderers.Add(renderer);
+                break;
+
+            case ColorType.Bumper:
+                bumperPartRenderers.Add(renderer);
+                break;
         }
     }
 }
