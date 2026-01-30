79e52410475c39cae1471ce48c21cd82cc2a8f4c
TimoKuronen <timojkuronen@gmail.com>
2026-01-30 20:02:20 +0200

    Pause menu stuff

--- Assets/Limekicker/Scripts/UI/GameHUD.cs
+++ Assets/Limekicker/Scripts/UI/GameHUD.cs
@@ -1,9 +1,46 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class GameHUD : MonoBehaviour
 {
+    public static GameHUD Instance { get; private set; }
+    public TextMeshProUGUI startCounterText;
+    public TextMeshProUGUI GoText;
+    public GameObject pauseMenu;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
     public void LeaveGame()
     {
         NetworkSession.LeaveGame();
+    }
+
+    public IEnumerator AnimateGoText()
+    {
+        GoText.gameObject.SetActive(true);
+        float timer = 0f;
+        while (true)
+        {
+            if (timer > 1f)
+                break;
+
+            // pump scale up and down
+            GoText.transform.localScale = Vector3.one * (1f + 0.5f * Mathf.Sin(timer * 5f));
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        GoText.gameObject.SetActive(false);
+        GoText.transform.localScale = Vector3.one;
     }
 }
---
+++ Assets/Limekicker/Scripts/UI/GameMenu.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField] private Button sfxButton;
+    [SerializeField] private Button musicButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        sfxButton.onClick.AddListener(ToggleSFX);
+        musicButton.onClick.AddListener(ToggleMusic);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void ToggleSFX()
+    {
+  
+    }
+
+    private void ToggleMusic()
+    {
+        
+    }
+
+    private void QuitGame()
+    {
+        
+    }
+}
